089fad2d9a4bbed5d50230a9563247400f9d1c7c
Michael Stephanou <mmstephanou@gmail.com>
2022-03-23 17:06:29 +0000

    server up and running

--- config/environment.js
+++ config/environment.js
@@ -1,1 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const port = process.env.PORT || 3000;
+
+export const dbURL = process.env.dbURL || 'mongodb://127.0.0.1:27017/project-3';
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { port } from './config/environment.js';
+import router from './config/router.js';
+import { connectToDb } from './db/helpers.js';
 
 const app = express();
 app.use(express.json());
