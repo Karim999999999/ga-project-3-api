17cd9d2a6c0bd244af5eeef6678aa849a4639047
kemo99 <karim.ali@live.com>
2022-03-25 09:58:16 +0000

    updated users, sessions, login, athletes, medical incidents

--- config/router.js
+++ config/router.js
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import secureRoute from '../middleware/secureRoute.js';
 import {
   getAllUsers,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
+  loginUser,
 } from '../controllers/userController.js';
 
 import {
@@ -26,8 +27,56 @@ router
   .put(updateArticle)
   .delete(deleteArticle);
 
-router.route('/users').get(getAllUsers).post(createUser);
-
+router.route('/users').get(getAllUsers);
+router.route('/register').post(createUser);
+router.route('/login').put(loginUser);
 router.route('/users/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// Router For Articles
+import {
+  getAllAthletesPrivate,
+  getApprovedAthletesPublic,
+  getAthletesByStatusPrivate,
+  getAthletesByIdPrivate,
+  getAthletesByIdPublic,
+  editAthleteDetailsbyIdPrivate,
+  deleteAthletePrivate,
+  getAthletesAttendancePrivate,
+  searchAthleteByNameApprovedAthletesPublic,
+  searchAthleteByNameAllAthletesPrivate,
+  getAthletesMedicalIncidentsPrivate,
+  createNewAthlete,
+} from '../controllers/athleteController.js';
+
+router
+  .route('/athlete')
+  .get(secureRoute, getAllAthletesPrivate)
+  .post(createNewAthlete);
+router.route('/approved-athlete').get(getApprovedAthletesPublic);
+router.route('/athlete/:status').get(secureRoute, getAthletesByStatusPrivate);
+router
+  .route('/athlete/:id')
+  .get(secureRoute, getAthletesByIdPrivate)
+  .get(getAthletesByIdPublic)
+  .put(secureRoute, editAthleteDetailsbyIdPrivate)
+  .delete(secureRoute, deleteAthletePrivate);
+router
+  .route('/approved-athlete/name/:searchName')
+  .get(secureRoute, searchAthleteByNameAllAthletesPrivate)
+  .get(searchAthleteByNameApprovedAthletesPublic);
+router
+  .route('/athlete/:id/medical-incidents')
+  .get(secureRoute, getAthletesMedicalIncidentsPrivate);
+router
+  .route('/athlete/:id/attendance')
+  .get(secureRoute, getAthletesAttendancePrivate);
+
+// Router For Athletes
+
+// Router For Medical Incidents
+
+// Router For Photos
+
+// Router For Sessions
 
 export default router;
